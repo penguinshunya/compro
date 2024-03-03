7cc11a1d8544e94d27cb983c2b8dfb65794948af
penguinshunya <takayashunya@gmail.com>
2024-03-03 12:38:55 +0900

    add abc311

--- main.cpp
+++ main.cpp
@@ -1,36 +1,32 @@
 #include "library/main.hpp"
 
 void solve() {
-  int N;
-  cin >> N;
-  vector<vector<int>> to(N);
-  rep(i, N - 1) {
-    int u, v;
-    cin >> u >> v;
-    u--, v--;
-    to[u].emplace_back(v);
-    to[v].emplace_back(u);
+  int H, W, N;
+  cin >> H >> W >> N;
+  auto holes = vector(H + 1, vector(W + 1, 0));
+  rep(i, N) {
+    int a, b;
+    cin >> a >> b;
+    holes[a][b] += 1;
+  }
+  reps(i, H) reps(j, W) {
+    holes[i][j] += holes[i - 1][j] + holes[i][j - 1] - holes[i - 1][j - 1];
   }
   i64 ans = 0;
-  auto dfs = [&](auto dfs, int u, int p) -> int {
-    vector<int> sums;
-    for (auto v : to[u]) {
-      if (v == p) continue;
-      sums.push_back(dfs(dfs, v, u));
+  reps(i, H) reps(j, W) {
+    int ok = 0, ng = 3010;
+    while (ng - ok != 1) {
+      int md = (ok + ng) / 2;
+      if (i - md < 0 || j - md < 0) {
+        ng = md;
+        continue;
+      }
+      int pi = i - md, pj = j - md;
+      int cnt = holes[i][j] - holes[i][pj] - holes[pi][j] + holes[pi][pj];
+      if (cnt == 0) ok = md;
+      else ng = md;
     }
-    int su = accumulate(all(sums), 0);
-
-    sums.push_back(N - su - 1);
-    vector<i64> dp_(4);
-    dp_[0] = 1;
-    for (auto sum : sums) {
-      rreps(i, 3) dp_[i] += dp_[i - 1] * sum;
-    }
-    ans += dp_[3];
-    sums.pop_back();
-
-    return su + 1;
-  };
-  dfs(dfs, 0, -1);
+    ans += ok;
+  }
   cout << ans << endl;
 }
