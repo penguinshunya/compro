fc252aba77fc5a2148e23efedd454fbd744d2735
penguinshunya <takayashunya@gmail.com>
2024-03-06 04:04:38 +0900

    斜め移動は見通しが悪いので横移動で考えると最短経路の場合の数になる、という天才的発想

---
+++ atcoder/abc311/f.cpp
@@ -0,0 +1,133 @@
+#line 1 "library/main.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using f80 = long double;
+using vi64 = vector<i64>;
+using vf80 = vector<f80>;
+using vstr = vector<string>;
+
+#define rep(i, n) for (int i = 0; i < int(n); i++)
+#define rrep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define reps(i, n) for (int i = 1; i <= int(n); i++)
+#define rreps(i, n) for (int i = int(n); i >= 1; i--)
+#define repc(i, n) for (int i = 0; i <= int(n); i++)
+#define rrepc(i, n) for (int i = int(n); i >= 0; i--)
+#define repi(i, a, b) for (int i = int(a); i < int(b); i++)
+#define rrepi(i, a, b) for (int i = int(b) - 1; i >= int(a); i--)
+#define repic(i, a, b) for (int i = int(a); i <= int(b); i++)
+#define rrepic(i, a, b) for (int i = int(b); i >= int(a); i--)
+#define all(a) (a).begin(), (a).end()
+
+const int INF = 1001001001;
+const i64 LINF = 1001001001001001001ll;
+const int dx[] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+i64 gcd(i64 a, i64 b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+i64 lcm(i64 a, i64 b) { return a / gcd(a, b) * b; }
+
+pair<i64, i64> extgcd(i64 a, i64 b) {
+  if (b == 0) return make_pair(1, 0);
+  auto [y, x] = extgcd(b, a % b);
+  y -= a / b * x;
+  return make_pair(x, y);
+}
+
+template <typename T> inline T power(T x, i64 n, T e = 1) {
+  T r = e;
+  while (n > 0) {
+    if (n & 1) r *= x;
+    n >>= 1, x *= x;
+  }
+  return r;
+}
+
+template <typename T, typename S> inline bool chmax(T &a, const S &b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+
+template <typename T, typename S> inline bool chmin(T &a, const S &b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+template <typename T> istream &operator>>(istream &is, vector<T> &v) {
+  for (auto &x : v)
+    is >> x;
+  return is;
+}
+
+template <typename T> ostream &operator<<(ostream &os, vector<T> &v) {
+  rep(i, v.size()) {
+    if (i) os << ' ';
+    os << v[i];
+  }
+  return os;
+}
+
+template <typename T, typename S>
+istream &operator>>(istream &is, pair<T, S> &p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template <typename T, typename S>
+ostream &operator<<(ostream &os, pair<T, S> &p) {
+  os << p.first << ' ' << p.second;
+  return os;
+}
+
+inline unsigned long long xorshift() {
+  static unsigned long long x = 88172645463325252ull;
+  x = x ^ (x << 7);
+  return x = x ^ (x >> 9);
+}
+
+void solve();
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << fixed << setprecision(16);
+  solve();
+  return 0;
+}
+#line 2 "main.cpp"
+
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+  vstr S(N);
+  cin >> S;
+  vstr T(N + M - 1, string(M, '.'));
+  rep(j, M) {
+    rep(i, N) {
+      int ni = (M - 1 - j) + i;
+      T[ni][j] = S[i][j];
+    }
+  }
+  rep(i, N + M - 1) rep(j, M) {
+    if (i > 0 && T[i - 1][j] == '#') T[i][j] = '#';
+    if (j > 0 && T[i][j - 1] == '#') T[i][j] = '#';
+  }
+  rep(j, M) rep(i, M - j - 1) T[i][j] = 'x';
+  rep(j, M) rep(i, j) T[N + M - 2 - i][j] = '#';
+  auto dp = vector(N + M + 1, vector(M + 1, mint(0)));
+  dp[0][M] = 1;
+  rrepc(j, M) rep(i, N + M) {
+    if (j >= 1 && (i == N + M - 1 || T[i][j - 1] != 'x'))
+      dp[i][j - 1] += dp[i][j];
+    if (j == 0 || (i <= N + M - 2 && T[i][j - 1] != '#'))
+      dp[i + 1][j] += dp[i][j];
+  }
+  cout << dp[N + M - 1][0].val() << endl;
+}
