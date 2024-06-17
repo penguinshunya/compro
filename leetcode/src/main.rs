61d3b166a95089a21c4933a77418a4375e1d30b4
penguinshunya <takayashunya@gmail.com>
2024-06-18 06:09:35 +0900

    HashMapの実験

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    // HashMap::insert は既存の値を上書きするか？
+    {
+        let mut mp: HashMap<i32, usize> = HashMap::new();
+        let v = vec![1, 2, 2];
+        for (i, x) in v.into_iter().enumerate() {
+            mp.insert(x, i);
+        }
+        print!("{{ ");
+        for (k, v) in mp.into_iter() {
+            print!("{}:{} ", k, v); // { 1:0 2:2 }
+        }
+        println!("}}");
+    }
+}
