177a5bc51ed77f5509d6ae2fd58b95e12177c8bd
penguinshunya <takayashunya@gmail.com>
2024-06-25 23:59:50 +0900

    devcontainerの設定

--- .devcontainer/my.bash
+++ .devcontainer/my.bash
@@ -1,5 +1,4 @@
 function c() {
-  # oj-bundle -I/workspaces/compro $1 > bundle.cpp
   oj-bundle -I/workspaces/compro main.cpp > bundle.cpp
   c++ -std=c++20 -O2 -I/workspaces/compro/ac-library bundle.cpp
 }
--- leetcode/MEMO.md
+++
@@ -1,3 +0,0 @@
-# Rust メモ
-
-- C++ の unordered_map の代わりは `std::collections::HashMap` 。これがとても速そう
